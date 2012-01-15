803cd6a0a9bef48e8b9c59329dd965cea11df07a
bitrut <bitrut@gmail.com>
2012-01-15 23:42:08 +0100

    New audio. Parameters tuning.

--- starwars.js
+++ starwars.js
@@ -4,16 +4,15 @@ StarWars.startAnimation = function() {
   var content = $("#content")
   var h = $(content).height();
   $(content).css('height', 200).css('top', window.innerHeight);
-  $(content).animate({top: -2*h, height: 2*h}, h*20, 'linear', function(){
+  $(content).animate({top: -2*h, height: 2.2*h}, h*20, 'linear', function(){
     $(this).fadeOut();
   });
 }
   
+$(document).ready(function(){
   var audio = document.getElementById("song");
   audio.addEventListener("playing", function() {
     StarWars.startAnimation();
   });
   audio.play();
 });
-
-
