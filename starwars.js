e0a41c7d8c4413107e67bb9ec872989bfd6d8256
bitrut <bitrut@gmail.com>
2012-01-15 23:21:16 +0100

    Clean up after refactoring

--- starwars.js
+++ starwars.js
@@ -8,25 +8,12 @@ StarWars.startAnimation = function() {
     $(this).fadeOut();
   });
 }
-
-
-$(document).ready(function() {
-  /*StarWars.trueHeight = ( function(){
-    var $tempobj = $('#textContainer') // starting with truncated text div container
-      .clone().contents() // duplicate the text
-      .wrapAll('<div id="content"/>') // wrap it in a container
-      .parent().appendTo('#scene') // add this to the dom
-      .css('top','-1000px'); // but put it far off-screen
-    var result = $tempobj.height(); // measure it
-    $tempobj.remove(); // clean up
-    return result;
-  })();*/
   
   var audio = document.getElementById("song");
   audio.addEventListener("playing", function() {
     StarWars.startAnimation();
   });
-  //audio.play();
+  audio.play();
   StarWars.startAnimation();
 });
 
